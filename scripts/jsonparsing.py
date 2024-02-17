9ee972b525bf58a1ba4659a50202013b64133441
Abu Hamza <abuhamza2308@hotmail.com>
2024-02-17 19:13:36 +0000

    update

--- scripts/jsonparsing.py
+++ scripts/jsonparsing.py
@@ -2,19 +2,16 @@ import json
 from collections import Counter
 
 # path to one json file from the data directory
-json_path = 'data\Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json'
+json_path = 'data\\Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json'
 
 # extract json data
-with open(json_path, 'r') as f:
-    data = json.load(f)
-    entries = data['entry']
+def extract_data(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        for entry in data['entry']:
+            resource = entry['resource']
+            yield resource
 
-resource_types = (entry for entry in entries)
-print(type(resource_types))
-    
-# element_count = Counter(resource_types)
-# for element, count in element_count.items():
-#     print(f"{element}: {count}")
-    
+# select 'patient' entry of the file
+resource = extract_data(json_path)
 
-    
--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -1,19 +1,25 @@
 import json
 
 # Generator function yielding all data for each json file in the directory
-def extract_data(json_files):
+def extract_data(json_files: list):
     """ Extracts all data from the JSON files in the directory.
     
         Parameters:
-            json_files (list): The path to the JSON files. 
+            json_files (list): List of paths of json files. 
         
         Yields:
-            extracted_data (generator object): Generator containing data from all json files in the directory.
+            extracted_data (generator object): Generator object containing data from all json files in the directory.
     """
     # Yields all 'resource' from each entry for each patient
     for json_file in json_files:
-        with open(json_file, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            for entry in data['entry']:
-                resource = entry['resource']
-                yield resource
+        try:
+            with open(json_file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                for entry in data['entry']:
+                    resource = entry['resource']
+                    yield resource
+        except FileNotFoundError:
+            print(f"Error: File {json_file} not found")
+        except json.JSONDecodeError:
+            print(f"Error: Unable to decode JSON in file {json_file}")
+            
