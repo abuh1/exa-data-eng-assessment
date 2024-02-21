842e5db8a8031f3f3d051ea7a76cc7e49b61a9ca
Abu Hamza <abuhamza2308@hotmail.com>
2024-02-21 17:37:43 +0000

    update

--- src/pipeline/transform_test.py
+++ src/pipeline/transform_test.py
@@ -1,8 +1,12 @@
 import os
 import sys
+import time
 sys.path.append('Z:\\Documents\\projects\\exa-data-eng-assessment')
 from src.pipeline.extract import extract_data
 from src.pipeline.transform import transform_data
+from src.pipeline.load import load_to_postgres
+
+start_time = time.time()
 
 main_path = os.listdir('data')
 json_paths = ['data\\' + p for p in main_path]
@@ -10,4 +14,5 @@ json_paths = ['data\\' + p for p in main_path]
 extracted = extract_data(json_paths)
 
 resources = transform_data(extracted)
-print(resources[0].keys())
+
+load_to_postgres(resources)
