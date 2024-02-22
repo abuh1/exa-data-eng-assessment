fda90f818ad339d9fa2fdb2b6343cb5d408a68af
Abu Hamza <abuhamza2308@hotmail.com>
2024-02-22 11:27:03 +0000

    update

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# PostgreSQL credentials
+PSQL_CREDENTIALS = {
+    'dbname': 'health_resources',
+    'user': 'postgres',
+    'password': 'password',
+    'host': 'localhost',
+    'port': '5432'}
+
+# Data path
+PATH_TO_DATA = 'Z:\\Documents\\projects\\exa-data-eng-assessment\\data'
